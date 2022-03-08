f14acc6814cc735a3ae5f1a1a44af72575fe23e1
Senapati, Rasmiranjan <rasmiranjan.senapati@intel.com>
2022-03-08 01:23:21 +0000

    Rebase S_Staging branch bsp to 2022_ww07 Celadon Release
    Change-Id: Iec4fb5c03bf12e56186711bbc00aa3adf4326625
    Tracked-On: OAM-100906
    Signed-off-by: Senapati, Rasmiranjan <rasmiranjan.senapati@intel.com>

--- host/kernel/lts2020-chromium/build_weekly.sh
+++ host/kernel/lts2020-chromium/build_weekly.sh
@@ -5,7 +5,7 @@ mkdir -p host_kernel
 cd host_kernel
 git clone https://github.com/projectceladon/linux-intel-lts2020-chromium.git
 cd linux-intel-lts2020-chromium
-git checkout 3fb1b64aaaa2073fdf79e823623d68274778aa47
+git checkout 6dda9a5b0c342d16e4cde6fab186a89595de5095
 cp ../../x86_64_defconfig .config
 patch_list=`find ../../ -iname "*.patch" | sort -u`
 for i in $patch_list
