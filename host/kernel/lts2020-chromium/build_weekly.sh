6ca171ba5ba0034fec01ecf170bf1173d504084e
svenate <salini.venate@intel.com>
2022-07-20 14:37:25 +0530

    [BSP] Rebase to ww31-2022 of celadon release
    Rebasing Android T-staging branch on ww31-2022
    of celadon release
    
    Tracked-On: OAM-102861
    Signed-off-by: svenate <salini.venate@intel.com>

--- host/kernel/lts2020-chromium/build_weekly.sh
+++ host/kernel/lts2020-chromium/build_weekly.sh
@@ -5,7 +5,7 @@ mkdir -p host_kernel
 cd host_kernel
 git clone https://github.com/projectceladon/linux-intel-lts2020-chromium.git
 cd linux-intel-lts2020-chromium
-git checkout 32f6e0a8092694a4f801dcc013a2a34af4563b3a
+git checkout 4d29e1cee847ff001c7e4439dd893c6df6164868
 cp ../../x86_64_defconfig .config
 patch_list=`find ../../ -iname "*.patch" | sort -u`
 for i in $patch_list
